202e1a2c196ad0f4b8c11fd2a9ee80e3599b7f7f
Rob Haley <rob_haley@live.co.uk>
2015-05-15 10:40:51 +0100

    implemented largest palindrome product

--- src/main/scala/Problem4.scala
+++ src/main/scala/Problem4.scala
@@ -2,4 +2,14 @@ package problem4
 
 object LargestPalindromeProduct {
     def isPalindrome(x: Int) = x.toString.reverse == x.toString
+    def ofNumbersBetween(x: Int, y: Int) = {
+        val max = math.max(x, y)
+        val min = math.min(x, y)
+        (for {
+            factor1 <- max to min by -1
+            factor2 <- factor1 to min by -1
+            product = factor1 * factor2
+            if(isPalindrome(product))
+        } yield product).max
+    }
 }
--- src/test/scala/Problem4Tests.scala
+++ src/test/scala/Problem4Tests.scala
@@ -9,4 +9,12 @@ class Problem4Spec extends FlatSpec with Matchers {
     "isPalindrome" should "be false for 23" in {
         LargestPalindromeProduct.isPalindrome(23) should be (false)
     }
+
+    "ofNumbersBetween" should "be 9009 for 10 & 99" in {
+        LargestPalindromeProduct.ofNumbersBetween(10, 99) should be (9009)
+    }
+
+    "ofNumbersBetween" should "be 99 for 1 & 10" in {
+        LargestPalindromeProduct.ofNumbersBetween(1, 10) should be (9)
+    }
 }
